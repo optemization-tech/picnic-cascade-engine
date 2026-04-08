278b60eb818fbc8065311890a082487efeecd53e
Seb <sebastien@picnichealth.com>
2026-04-08 13:15:08 -0700

    Use UUID webhook secret

--- src/webhook-secret.js
+++ src/webhook-secret.js
@@ -1,2 +1,2 @@
 // TODO: move this secret into GCS-backed app config when this code moves into the Picnic repo.
-export const REPO_WEBHOOK_SECRET = 'picnic-cascade-engine-dev-webhook-secret-2026-04';
+export const REPO_WEBHOOK_SECRET = '4f7f9d20-5af3-4e5e-94ae-f7e34d4f0df4';
